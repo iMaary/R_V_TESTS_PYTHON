e8cccbfd673d81b15a591fa19ffcf9ee3d36998d
Aluno <m.rodriguesnave@gmail.com>
2022-05-27 17:35:42 -0300

    refactoring tests and folders architecture

--- tests/test_calculator_a.py
+++ tests/test_calculator_a.py
@@ -8,6 +8,30 @@ def test_sum(a, b, res):
     assert calculator.sum(a, b) == res
 
 
+@pytest.mark.parametrize("a, b, res", [(1, 1, 1), (2, 3, 6)])
+def test_mult(a, b, res):
+    calculator = CalculatorA()
+    assert calculator.mult(a, b) == res
+
+
+@pytest.mark.parametrize("a, b, res", [(2, 2, 1), (10, 2, 5)])
+def test_div(a, b, res):
+    calculator = CalculatorA()
+    assert calculator.div(a, b) == res
+
+
+@pytest.mark.parametrize("a, b, res", [(7, 5, 2), (8, 3, 5)])
+def test_subtract(a, b, res):
+    calculator = CalculatorA()
+    assert calculator.sub(a, b) == res
+
+
+@pytest.mark.parametrize("a, b, res", [(3, 2, 9), (2, 2, 4)])
+def test_pot(a, b, res):
+    calculator = CalculatorA()
+    assert calculator.pot(a, b) == res
+
+
 @pytest.mark.parametrize("a, b, wrong_result", [(1, 1, 3), (4, 4, 6)])
 def test_sum_diff_results(a, b, wrong_result):
     calc = CalculatorA()

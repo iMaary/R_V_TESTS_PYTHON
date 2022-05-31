8379ecbdbcf47af5c5706db00b4011f5a6792da3
Aluno <m.rodriguesnave@gmail.com>
2022-05-31 10:05:26 -0300

    fixing parameters tests

--- tests/test_calculator_a.py
+++ tests/test_calculator_a.py
@@ -37,15 +37,18 @@ def test_sum_diff_results(a, b, wrong_result):
     calc = CalculatorA()
     assert calc.sum(a, b) != wrong_result
 
+
 @pytest.mark.parametrize("a, b, wrong_result", [(4, 8, 10), (4, 42, 0)])
 def test_sub_diff_results(a, b, wrong_result):
     calc = CalculatorA()
     assert calc.sub(a, b) != wrong_result
 
+
 @pytest.mark.parametrize("a, b, wrong_result", [(56, 8, 7), (1, 0, 6)])
 def test_mult_diff_results(a, b, wrong_result):
     calc = CalculatorA()
     assert calc.mult(a, b) != wrong_result
+
 
 @pytest.mark.parametrize("a, b, wrong_result", [(2, 4, 5), (20, 4, 6)])
 def test_pot_diff_results(a, b, wrong_result):
@@ -59,47 +62,71 @@ def test_pot_diff_results(a, b, wrong_result):
                              ("4", "4", 8),
                              ("1", 2, 3)
                          ])
-
 def test_sum_strings(a, b, result):
     calc = CalculatorA()
     with pytest.raises(CalculatorError):
         calc.sum(a, b)
 
+
+@pytest.mark.parametrize("a, b, result",
+                         [
+                             (5, "2", 3),
+                             ("4", "4", 0),
+                             ("2", 1, 1)
+                         ])
 def test_sub_strings(a, b, result):
     calc = CalculatorA()
     with pytest.raises(CalculatorError):
         calc.sub(a, b)
 
+
+@pytest.mark.parametrize("a, b, result",
+                         [
+                             (5, "2", 10),
+                             ("4", "4", 16),
+                             ("1", 2, 2)
+                         ])
 def test_mult_strings(a, b, result):
     calc = CalculatorA()
     with pytest.raises(CalculatorError):
-        calc.mult(a, b) 
+        calc.mult(a, b)
 
+
+@pytest.mark.parametrize("a, b, result",
+                         [
+                             (5, "2", 25),
+                             ("2", "3", 8),
+                             ("1", 2, 1)
+                         ])
 def test_pot_strings(a, b, result):
     calc = CalculatorA()
     with pytest.raises(CalculatorError):
         calc.pot(a, b)
 
+
 def sum_stress_test(a, b, result):
-    calc = CalculatorA();
+    calc = CalculatorA()
     a = 999999999999
     b = 999999999999
     assert calc.sum(a, b) == result
+
 
 def sub_stress_test(a, b, result):
-    calc = CalculatorA();
+    calc = CalculatorA()
     a = 999999999999
     b = 999999999999
     assert calc.sum(a, b) == result
+
 
 def mult_stress_test(a, b, result):
-    calc = CalculatorA();
+    calc = CalculatorA()
     a = 999999999999
     b = 999999999999
     assert calc.sum(a, b) == result
 
+
 def pot_stress_test(a, b, result):
-    calc = CalculatorA();
+    calc = CalculatorA()
     a = 999999999999
     b = 999999999999
     assert calc.sum(a, b) == result
